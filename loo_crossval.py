cd2daeaf51aad015d2f99584e9d1fb06fc8a2bd3
Andrew Smith <andrewsmith_97@yahoo.com>
2024-07-03 18:31:13 -0400

    Updated to use Gemini models, and to do extra stuff I had written loo_crossval_subset.py to do

--- loo_crossval.py
+++ loo_crossval.py
@@ -2,6 +2,11 @@ import siteminder
 import json
 import sys
 import utils
+import math
+import time
+
+QUERIES_PER_MINUTE = 15
+SLEEP_TIME = math.ceil(60 / QUERIES_PER_MINUTE) + 1
 
 USE_SITEMINDER=False
 
@@ -14,7 +19,7 @@ if USE_SITEMINDER:
         print("Error: " + login_resp['msg'])
         sys.exit()
 
-base_url = "http://ip-172-25-132-16.rdcloud.bms.com:5008/"
+base_url = "http://canvasxpress.org:5008/"
 
 get_all_few_shots_url = base_url + "get_few_shots?num=all&format=json"
 ask_url = base_url + "ask"
@@ -23,12 +28,15 @@ if USE_SITEMINDER:
     fetch_resp = siteminder.sm_fetch(get_all_few_shots_url,cdict=login_resp['cdict'])
 else:
     fetch_resp = siteminder.fetch(get_all_few_shots_url)
+time.sleep(SLEEP_TIME)
 fetch_json_txt = fetch_resp['text']
 allFewShotsList = json.loads(fetch_json_txt)
 
 totalFewShotCt = len(allFewShotsList)
 similarityScores = []
 exactMatchCt = 0
+subsetCt = 0
+minSimilarityScore = 100.0
 
 for curFewShotObj in allFewShotsList:
     question = curFewShotObj['English Text']
@@ -39,9 +47,11 @@ for curFewShotObj in allFewShotsList:
     print("FEW SHOT QUESTION: " + question)
     print("FEW SHOT DATAFILE_CONTENTS: " + datafile_contents)
     print("FEW SHOT ANSWER: " + answer)
+    print("FEW SHOT ANSWER PRETTY:")
+    print(json.dumps(answerObj,indent=2,sort_keys=True))
 
     postData = { 'datafile_contents': datafile_contents,
-                 'model': 'gpt-4-32k',
+                 'model': 'gemini-1.5-flash',
                  'topp': 1,
                  'temperature': 0,
                  'presence_penalty': 0,
@@ -56,22 +66,39 @@ for curFewShotObj in allFewShotsList:
     ask_json_txt = ask_resp['text']
     askJsonObj = json.loads(ask_json_txt)
     jsonSimilarityScore = 0.0
+    answerIsSubset = "Unknown"
     if askJsonObj['success'] and 'config' in askJsonObj:
         jsonSimilarityScore = utils.json_similarity(answerObj,askJsonObj['config'])
+        if utils.is_subset(answerObj,askJsonObj['config']):
+            answerIsSubset = "True"
+            subsetCt = subsetCt + 1
+        else:
+            answerIsSubset = "False"
+    if jsonSimilarityScore < minSimilarityScore:
+        minSimilarityScore = jsonSimilarityScore
     print("LLM GENERATED RESPONSE: " + ask_json_txt)
+    print("LLM ANSWER PRETTY:")
+    print(json.dumps(askJsonObj['config'],indent=2,sort_keys=True))
     print("SIMILARITY SCORE: " + str(jsonSimilarityScore))
+    print("TRUE ANSWER IS SUBSET OF LLM GENERATED ANSWER: " + answerIsSubset)
     similarityScores.append(jsonSimilarityScore)
     if jsonSimilarityScore >= 100.0:
         exactMatchCt = exactMatchCt + 1
+    time.sleep(SLEEP_TIME)
+
 
 avgSimilarityScore = sum(similarityScores) / len(similarityScores)
 percentExactMatch = exactMatchCt / float(len(similarityScores))
+percentSubsetMatch = subsetCt / fewShotCt
 
 print("***SUMMARY***")
 print("NUMBER OF FEW SHOTS: " + str(totalFewShotCt))
 print("NUMBER OF EXACT MATCHES: " + str(exactMatchCt))
 print("PERCENT EXACT MATCHES: " + str(percentExactMatch))
+print("NUMBER OF SUBSET MATCHES: " + str(subsetCt))
+print("PERCENT SUBSET MATCHES: " + str(percentSubsetMatch))
 print("AVERAGE SIMILARITY SCORE: " + str(avgSimilarityScore))
+print("MINIMUM SIMILARITY SCORE: " + str(minSimilarityScore))
 
 ##Args to /ask:
 #datafile_upload: (binary) --- or alternatively pass datafile_contents as row of rows, e.g. [['header1','header2',...],[1,2,....]]
