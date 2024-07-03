54f2ed1827447c3d5cf77f5fbc67d4d73c0d672c
EC2 Default User <ec2-user@ip-172-25-132-16.rdcloud.bms.com>
2024-07-02 21:17:12 -0400

    Lots of refactoring, also reverted to BMS version that doesn't work with Google's LLMs, need to modify so they do

--- app.py
+++ app.py
@@ -1,59 +1,30 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, abort, session, jsonify, send_from_directory, send_file
 from werkzeug.utils import secure_filename
-import csv
 import os
 import re
 import datetime
 import time
 import json
 import utils
-#import llm_logging
+import llm
+import llm_logging
 import urllib.parse
 import boto3
 from tabulate import tabulate
 from AESCipher import AESCipher
-from FlagEmbedding import BGEM3FlagModel
-from pymilvus import MilvusClient
-from pymilvus.model.hybrid import BGEM3EmbeddingFunction
 import numpy as np
-import tiktoken
-import google.generativeai as genai
-from dotenv import load_dotenv
-
-load_dotenv()
 
 code_release = "https://biogit.pri.bms.com/smitha26/LLM-testing"
 LOGCHATS=False
 
-USE_MODEL="google_gemini" #"titan" or "openai"
-
-google_api_key = os.environ.get("GOOGLE_API_KEY")
-
-import openai
-# OpenAI Azure Config
-openai.api_type = os.environ.get('OPENAI_API_TYPE')
-openai.api_key = os.environ.get('OPENAI_API_KEY')
-openai.api_base = os.environ.get('OPENAI_API_BASE')
-openai.api_version = os.environ.get('OPENAI_API_VERSION')
-
-bge_m3_ef = BGEM3EmbeddingFunction(
-    model_name='BAAI/bge-m3', # Specify the model name
-    device='cpu', # Specify the device to use, e.g., 'cpu' or 'cuda:0'
-    use_fp16=False # Specify whether to use fp16. Set to `False` if `device` is `cpu`.
-)
+USE_MODEL="openai" #"titan" or "openai"
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = utils.random_password(16)
 app.config['UPLOAD_FOLDER'] = "/tmp"
 aes = AESCipher(app.config['SECRET_KEY'])
 
-def getMilvusClient():
-    if 'MILVUS_CLIENT' not in app.config:
-        fewShotDbLoc = "/root/.cache/canvasxpress_llm.db"
-        app.config['MILVUS_CLIENT'] = MilvusClient(fewShotDbLoc)
-    return(app.config['MILVUS_CLIENT'])
-
-SMVAL=False
+SMVAL=True
 validatedCookies = {}
 
 #See here: https://pythonise.com/series/learning-flask/python-before-after-request
@@ -110,103 +81,30 @@ def getenv():
         allEnvTxt = allEnvTxt + "{0}: {1}".format(name, value) + "\n"
     return("<plaintext>" + allEnvTxt + "</plaintext>")
 
-def getFewShots(prompt,numFewShots=25):
+@app.route('/get_few_shots',methods=['GET','POST'])
+def get_few_shots():
 
-    queries = [prompt]
+    prompt = request.values.get('prompt')
+    if prompt is not None:
+        prompt = prompt.strip()
+    num = request.values.get('num')
+    format = request.values.get('format')
+    filter_prompt = request.values.get('filter_prompt')
 
-    query_embeddings = bge_m3_ef.encode_queries(queries)
-    #collection_name="few_shot_examples",
-    #collection_name="canvasxpress_docs",
+    if format != 'text' and format != 'json':
+        format = 'text'
 
-    milvusClient = getMilvusClient()
-    res = milvusClient.search(
-        collection_name="few_shot_examples",
-        data=[query_embeddings["dense"][0]],
-        limit=numFewShots,
-        output_fields=["config", "configEnglish", "headers", "id"],
-    )
-    #English Text:...; Headers/Column Names: ..., Answer: ...
-    #See here for where got below to get the hits: https://github.com/milvus-io/pymilvus/blob/master/examples/milvus_client/simple.py#L68
-    fewShotTxt = ""
-    for hits in res:
-        for hit in hits:
-            curConfig = hit['entity']['config'].replace("\n"," ")
-            curEnglishConfig = hit['entity']['configEnglish'].replace("\n", " ")
-            curHeadersColumnNames = hit['entity']['headers'].replace("\n", " ")
-            fewShotTxt = fewShotTxt + f"English Text: {curEnglishConfig}; Headers/Column Names: {curHeadersColumnNames}, Answer: {curConfig}" + "\n"
+    milvusClient = llm.getMilvusClient(app)
+    if num == 'all':
+        resTxt = llm.getAllFewShots(milvusClient, format=format)
+    else:
+        if not utils.empty(filter_prompt) and filter_prompt == 'True':
+            filter_prompt = True
+        else:
+            filter_prompt = False
+        resTxt = llm.getFewShots(milvusClient, prompt,numFewShots=int(num),filterPrompt=filter_prompt,format=format)
 
-    return(fewShotTxt)
-
-def generate_prompt(canvasxpress_config_english, headers_column_names, schema_info_file="schema_info.txt", schema_info_string=None,
-                    prompt_file="prompt.md", few_shot_examples_file="few_shot_examples.txt", few_shot_examples_string=None):
-    with open(prompt_file, "r") as f:
-        prompt = f.read()
-
-    if few_shot_examples_string is None:
-        with open(few_shot_examples_file, "r") as f:
-            few_shot_examples_string = f.read()
-
-    if schema_info_string is None:
-        with open(schema_info_file, "r") as f:
-            schema_info_string = f.read()
-
-    prompt = prompt.format(
-        canvasxpress_config_english=canvasxpress_config_english, headers_column_names=headers_column_names, schema_info=schema_info_string,few_shot_examples=few_shot_examples_string
-    )
-
-    return prompt
-
-def generate_results_google_gemini(prompt,model='gemini-1.5-flash',temperature=0.0):
-
-    model = genai.GenerativeModel(model)
-    gen_config = genai.GenerationConfig(temperature=temperature,response_mime_type="application/json")
-    model_response = model.generate_content(prompt,generation_config=gen_config)
-    answer_txt = model_response.candidates[0].content.parts[0].text
-
-    return(answer_txt)
-
-def generate_results_titan(prompt, model='amazon.titan-tg1-large', max_token_count=2048, topp=1, temperature=0):
-
-    bedrockRuntime = boto3.client('bedrock-runtime')
-
-    body = json.dumps({
-        "inputText": prompt,
-        "textGenerationConfig": {
-            "maxTokenCount": max_token_count,
-            "stopSequences": [],
-            "temperature":temperature,
-            "topP":topp
-        }
-    })
-
-    response = bedrockRuntime.invoke_model(body=body, modelId=model, accept='application/json', contentType='application/json')
-    response_body = json.loads(response.get('body').read())
-    generated_text = response_body['results'][0]['outputText']
-
-    return(generated_text)
-
-def generate_results_openai(prompt, model='gpt-4-0314', max_new_tokens=500, topp=1.0, temperature=1.0, presence_penalty=0.0, frequency_penalty=0.0):
-
-    response = openai.ChatCompletion.create(
-        engine=model, # replace this value with the deployment name you chose when you deployed the associated model.
-        messages = [{"role":"user","content":prompt}],
-        temperature=temperature,
-        max_tokens=max_new_tokens,
-        top_p=topp,
-        frequency_penalty=frequency_penalty,
-        presence_penalty=presence_penalty,
-        stop=None)
-
-    generated_text = response['choices'][0]['message']['content']
-
-    return(generated_text)
-
-def parse_file(filename):
-    with open(filename, 'r') as f:
-        dialect = csv.Sniffer().sniff(f.read(1024))
-        f.seek(0)
-        reader = csv.reader(f, dialect)
-        return [row for row in reader]
+    return(resTxt)
 
 @app.route('/ask',methods=['GET','POST'])
 def ask():
@@ -214,12 +112,15 @@ def ask():
     datafilename = None
     datafile_contents = []
     headerRow = []
-    if 'datafile_upload' in request.files and request.files['datafile_upload'].filename != '':
+    in_datafile_contents = request.values.get('datafile_contents')
+    if not utils.empty(in_datafile_contents):
+        datafile_contents = json.loads(in_datafile_contents)
+    elif 'datafile_upload' in request.files and request.files['datafile_upload'].filename != '':
         datafile = request.files['datafile_upload']
         datafilename = secure_filename(datafile.filename)
         upload_filepath = os.path.join(app.config['UPLOAD_FOLDER'], datafilename)
         datafile.save(upload_filepath)
-        datafile_contents = parse_file(upload_filepath)
+        datafile_contents = utils.parse_file(upload_filepath)
     else:
         errResp = { 'text': "Error: you must upload a data file to visualize", 'success': False, 'config_generated_flag': False, }
         return(json.dumps(errResp))
@@ -236,7 +137,13 @@ def ask():
     temperature = request.values.get('temperature')
     presence_penalty = request.values.get('presence_penalty')
     frequency_penalty = request.values.get('frequency_penalty')
+    filter_prompt_from_few_shots = request.values.get('filter_prompt_from_few_shots')
     curDatetime = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
+
+    if not utils.empty(filter_prompt_from_few_shots) and filter_prompt_from_few_shots == 'True':
+        filter_prompt_from_few_shots = True
+    else:
+        filter_prompt_from_few_shots = False
 
     if utils.empty(prompt):
         errResp = { 'text': "Error: you must provide a description of the visualization you want", 'success': False, 'config_generated_flag': False, }
@@ -248,7 +155,7 @@ def ask():
         version = 'UNKNOWN'
 
     if utils.empty(model):
-        model = 'gemini-1.5-flash'
+        model = 'gpt-4-32k'
 
     if utils.empty(max_new_tokens):
         max_new_tokens = 1024
@@ -281,32 +188,27 @@ def ask():
                  'datetime': curDatetime }
     elif prompt == 'Help':
         resp = { 'success': True,
-                 'text': 'You can ask me for help with accessing the NGS360 database. You can search structured data by asking things like Can you give me a list of matching normal-tumor samples for the study CA209-214 or Which individuals in CA209-214 have both RNAseq and WES data?',
+                 'text': 'You can generate visualizations by describing them in plain English, e.g. boxplot with legend at top right, and uploading a file of data to graph.',
                  'config_generated_flag': False,
                  'prompt': orig_prompt,
                  'datetime': curDatetime }
     else:
 
-        fewShotTxt = getFewShots(prompt,numFewShots=25)
-        prompt = generate_prompt(prompt,str(headerRow), schema_info_file="/root/.cache/schema.txt",prompt_file="prompt.md",few_shot_examples_string=fewShotTxt)
+        milvusClient = llm.getMilvusClient(app)
+        fewShotTxt = llm.getFewShots(milvusClient, prompt,numFewShots=25,filterPrompt=filter_prompt_from_few_shots)
+        prompt = llm.generate_prompt(prompt,str(headerRow), schema_info_file="/root/.cache/schema.txt",prompt_file="prompt.md",few_shot_examples_string=fewShotTxt)
 
         generated_text = "NOTHINGYET"
         if USE_MODEL == "titan":
-            generated_text = generate_results_titan(prompt,model='amazon.titan-tg1-large', max_token_count=int(max_new_tokens), topp=float(topp), temperature=float(temperature))
+            generated_text = llm.generate_results_titan(prompt,model='amazon.titan-tg1-large', max_token_count=int(max_new_tokens), topp=float(topp), temperature=float(temperature))
         elif USE_MODEL == "openai":
-            generated_text = generate_results_openai(prompt, model=model, max_new_tokens=int(max_new_tokens),topp=float(topp),temperature=float(temperature),presence_penalty=float(presence_penalty),frequency_penalty=float(frequency_penalty))
-        elif USE_MODEL == 'google_gemini':
-            generated_text = generate_results_google_gemini(prompt,model=model,temperature=float(temperature))
+            generated_text = llm.generate_results_openai(prompt, model=model, max_new_tokens=int(max_new_tokens),topp=float(topp),temperature=float(temperature),presence_penalty=float(presence_penalty),frequency_penalty=float(frequency_penalty))
         if utils.empty(generated_text) or generated_text.strip() == "''":
             errResp = { 'text': "Error: No configuration was generated by the LLM, please try again.", 'success': False, 'config_generated_flag': False, }
             return(json.dumps(errResp))
         configObj = {}
         try:
-            #print("BEFORE: " + generated_text)
-            #generated_text.removeprefix('```json')
-            #generated_text.removesuffix('```')
-            #print("AFTER: " + generated_text)
-            configObj = json.loads(generated_text.strip())
+            configObj = json.loads(generated_text)
         except Exception as e:
             errResp = { 'text': "Error converting LLM generated config into JSON object: " + str(e) + ", raw result from LLM: " + generated_text, 'success': False, 'config_generated_flag': False, }
             return(json.dumps(errResp))
@@ -327,8 +229,8 @@ def ask():
 
     retValsJson = json.dumps(resp)
 
-    #if LOGCHATS:
-    #    llm_logging.log_chatinfo(code_release,version,"user","id",retValsJson)
+    if LOGCHATS:
+        llm_logging.log_chatinfo(code_release,version,"user","id",retValsJson)
 
     return(retValsJson)
 
